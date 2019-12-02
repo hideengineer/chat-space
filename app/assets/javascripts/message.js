976ffaca63e6a2ac79799ea373c1f4d1c2de854c
hideengineer <pingpong1993@outlook.jp>
2019-12-02 10:45:37 +0900

    非同期通信機能実装

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,54 @@
+$(function() {
+  function buildHTML(message) {
+    if (message.image) {
+      var html = `<div class="main__contents__box">
+                    <div class="main__contents__box__name">
+                      ${message.user_name}
+                    </div>
+                    <div class="main__contents__box__date">
+                      ${message.date}
+                    </div>
+                  </div>
+                  <div class="main__contents__message">
+                    ${message.content}
+                    <img class="lower-message__image" src=${message.image}>
+                  </div>`
+    } else {
+      var html = `<div class="main__contents__box">
+                    <div class="main__contents__box__name">
+                      ${message.user_name}
+                    </div>
+                    <div class="main__contents__box__date">
+                      ${message.date}
+                    </div>
+                  </div>
+                  <div class="main__contents__message">
+                    ${message.content}
+                  </div>`
+    }
+    return html
+  }
+  $('#new_message').on('submit', function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(message) {
+      var html = buildHTML(message);
+      $('.main__contents').append(html);
+      $('#new_message')[0].reset();
+      $('.main__form__input--submit').prop('disabled', false);
+      $('.main__contents').animate({ scrollTop: $('.main__contents')[0].scrollHeight});
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    })
+  })
+});
